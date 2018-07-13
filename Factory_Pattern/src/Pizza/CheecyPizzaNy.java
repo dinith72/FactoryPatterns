ea03e93058eebd1bd09039cf9d324d6bcf9cd215
Dinith Jayabodhi <jdinith72@gmail.com>
2018-07-13 17:31:58 +0530

    initail commit
    basic infrastructure of the factory pattern

---
+++ Factory_Pattern/src/Pizza/CheecyPizzaNy.java
@@ -0,0 +1,13 @@
+package Pizza;
+
+public class CheecyPizzaNy extends Pizza{
+
+    public CheecyPizzaNy() {
+        System.out.println("chhesy pizza new york");
+    }
+
+    @Override
+    void getName() {
+        System.out.println("chhesy pizza from new york");
+    }
+}
